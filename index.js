d0a2d2c759e8f5e87e4fc05870ce691d2d81eb1b
Mostafiz1257 <mostafiz1257@gmail.com>
2023-05-19 01:03:21 +0600

    insert post oparation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT ||5000
 const app = express();
@@ -9,7 +10,6 @@ app.use(cors())
 app.use(express())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esni35a.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +24,19 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+const toyCollection = client.db('toysDB').collection('toys')
+
+app.post('/toys',async(req,res)=>{
+  const addToys = req.body
+  console.log(addToys);
+
+})
+app.get('/toys',async(req,res)=>{
+  const cursor = toyCollection.find()
+  const result = await cursor.toArray();
+  res.send(result)
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
