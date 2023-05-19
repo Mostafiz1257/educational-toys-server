3d1f9bc1fac09a8c3d776987518b5ca09e7cb580
Mostafiz1257 <mostafiz1257@gmail.com>
2023-05-19 10:52:37 +0600

    added midlleware

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express =require('express')
+const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 const app = express();
 
 //middleware
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esni35a.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,18 +25,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-const toyCollection = client.db('toysDB').collection('toys')
+    const toyCollection = client.db('toysDB').collection('toys')
 
-app.post('/toys',async(req,res)=>{
-  const addToys = req.body
-  console.log(addToys);
+    app.post('/toys', async (req, res) => {
+      const addToys = req.body
+      console.log(addToys);
+      const result = await toyCollection.insertOne(addToys)
+      res.send(result)
 
-})
-app.get('/toys',async(req,res)=>{
-  const cursor = toyCollection.find()
-  const result = await cursor.toArray();
-  res.send(result)
-})
+    })
+    app.get('/toys', async (req, res) => {
+      const cursor = toyCollection.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -49,10 +51,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Buy some toys')
+app.get('/', (req, res) => {
+  res.send('Buy some toys')
 })
 
-app.listen(port,()=>{
-    console.log(`port is running ${port}`);
+app.listen(port, () => {
+  console.log(`port is running ${port}`);
 })
