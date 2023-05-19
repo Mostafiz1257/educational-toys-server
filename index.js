1589567299a73fc19f717ce12eea12bc9ee8804d
Mostafiz1257 <mostafiz1257@gmail.com>
2023-05-19 20:27:47 +0600

    added get single id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express();
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const toyCollection = client.db('toysDB').collection('toys')
 
     app.post('/toys', async (req, res) => {
@@ -39,6 +39,15 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result)
     })
+
+    app.get('/toys/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id : new ObjectId(id)}
+      const result = await toyCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
